19f71a8e2d31a6f2d865a3848c1afbd6e946b625
Hamza_Idb <97356530+Hamzaidb@users.noreply.github.com>
2023-12-13 17:06:34 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+// header container
+ScrollReveal().reveal(".header__container h1", {
+  ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".header__container p", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".header__container form", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".header__container a", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  autoplay: {
+    delay: 5000, // délai entre chaque changement de slide en millisecondes (par exemple 5000 pour 5 secondes)
+    disableOnInteraction: false, // empêche l'autoplay de s'arrêter lors d'interactions utilisateur (facultatif, selon vos préférences)
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
